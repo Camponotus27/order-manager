fca3030dd994045c354c801140dc0f8e966a42fd
Sebastian <sebastian.cornejo1@walmart.com>
2023-01-02 21:06:14 -0300

    add Relaod file go

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ type Response struct {
 func cookieAdd(c echo.Context) error {
 	u := &Response{
 		Message:       "Tu galleta fue añadida",
-		CurrentCookie: 12,
+		CurrentCookie: 13,
 	}
 	return c.JSON(http.StatusOK, u)
 }
